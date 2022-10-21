c8229faa149d76b0bb3a20203d35e50666304322
Masud Rana <masud.rana@apsissolutions.com>
2022-10-21 23:02:04 +0200

    Contain duplicate added

---
+++ Java/ContainsDuplicateII.java
@@ -0,0 +1,28 @@
+
+import java.util.HashMap;
+
+public class ContainsDuplicateII {
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i ++){
+            if (map.containsKey(nums[i]) && Math.abs(i - map.get(nums[i]))<= k){
+                return true;
+            }else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] number_list = {1,0,1,1};
+        int k = 1;
+
+        boolean result = containsNearbyDuplicate(number_list, k);
+
+        System.out.println(result);
+    }
+}
