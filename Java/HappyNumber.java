40827637404988b000c20fa362c1c460b3fc50ae
Masud Rana <masud.rana@apsissolutions.com>
2022-10-27 13:53:46 +0200

    new problem soved

---
+++ Java/HappyNumber.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class HappyNumber {
+
+    public static ArrayList<Integer> numberToDigit(int n){
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        while (n!=0){
+
+        }
+
+        return arr;
+    }
+
+    public static boolean isHappy(int n) {
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number= 19;
+        boolean result = isHappy(number);
+        System.out.println(numberToDigit(number));
+    }
+}
