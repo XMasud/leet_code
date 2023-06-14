96738dd74c5b3713eaff1e1503c9d024ff01aee6
Masud Rana <masud.rana@apsissolutions.com>
2023-06-14 16:51:08 +0200

    - problem 136 solved

--- Java/MoveZeroes283.java
+++ Java/MoveZeroes283.java
@@ -2,21 +2,21 @@ import java.util.Arrays;
 
 public class MoveZeroes283 {
 
-
     public static void moveZeroes(int[] nums) {
 
-        if(nums.length < 2){
-             int[] value = {0};
-             nums = value;
-        }else{
+        if (nums.length < 2) {
+            nums = new int[]{0};
+        } else {
+            //Tracking non zero element
             int index = 0;
-           for(int i=0; i< nums.length; i++){
-               if(nums[i] != 0){
-                   nums[index] = nums[i];
-                   index++;
-               }
-           }
-            for(int i=index; i< nums.length; i++){
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    nums[index] = nums[i];
+                    index++;
+                }
+            }
+            //Inserting 0
+            for (int i = index; i < nums.length; i++) {
                 nums[i] = 0;
             }
 
@@ -24,7 +24,7 @@ public class MoveZeroes283 {
     }
 
     public static void main(String[] args) {
-        int[] array = {0,1,0,3,12};
+        int[] array = {0, 1, 0, 3, 12};
         moveZeroes(array);
         System.out.println(Arrays.toString(array));
     }
