ea00b2e2c4583fc14a3d2dfa04556d50e48b5757
Masud Rana <masud.rana@apsissolutions.com>
2024-03-24 13:55:04 +0100

    New problem solved c

--- Java/HappyNumber202.java
+++ Java/HappyNumber202.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class HappyNumber202 {
+import java.util.HashSet;
+
+public class HappyNumber202 {
+
+    public static boolean isHappy(int n) {
+
+        HashSet<Integer> numbers = new HashSet<>();
+
+        while (true){
+            int sum = 0;
+            while (n > 0){
+                int digit = n % 10;
+                sum = sum + (digit * digit);
+                n = n / 10;
+            }
+
+            if (sum == 1)
+                return true;
+
+            n = sum;
+
+            if (numbers.contains(sum))
+                return false;
+            numbers.add(sum);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 19;
+        boolean result = isHappy(number);
+        System.out.println(result);
+    }
 }
--- Java/SellStock121.java
+++ Java/SellStock121.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class SellStock121 {
+public class SellStock121 {
+
+    public static int maxProfit(int[] prices) {
+
+        int maxProfit = 0;
+        int left = 0, right = 1;
+
+        System.out.println(prices.length);
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                System.out.println(prices[right] +" - "+ prices[left]);
+                int profit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                left = left + 1;
+            }
+            right += 1;
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] price = {1,2,4,2,5,7,2,4,9,0,9};
+        System.out.println("Result : "+maxProfit(price));
+    }
 }
