5b721e9f7c8236b702790e0fc1b50a2d806644db
Masud Rana <masud.rana@apsissolutions.com>
2024-03-29 16:47:56 +0100

    Trailing zero and pow problem solved

--- Java/Pow50.java
+++ Java/Pow50.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Pow50 {
+public class Pow50 {
+
+    public static double myPow(double x, int n) {
+
+        return Math.pow(x,n);
+    }
+    public static void main(String[] args) {
+        double x = 2.00000;
+        int n = -2;
+        double result = myPow(x, n);
+        System.out.println(result);
+    }
 }
