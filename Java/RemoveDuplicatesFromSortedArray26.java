8730a31e7d0ab6bd97c50195dbc7046218c01ac3
Masud Rana <masud.rana@apsissolutions.com>
2024-11-14 23:37:41 +0100

    Add binary problem

--- Java/RemoveDuplicatesFromSortedArray26.java
+++ Java/RemoveDuplicatesFromSortedArray26.java
@@ -1,18 +1,28 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class RemoveDuplicatesFromSortedArray26 {
 
     public static int removeDuplicates(int[] nums) {
 
+        int index = 1;
 
+        for (int i = 0; i < nums.length -1; i++) {
+            if(nums[i] != nums[i+1]){
+                nums[index] = nums[i+1];
+                index++;
+            }
+        }
 
-        return 0;
+        return index;
     }
 
     public static void main(String[] args) {
         int[]  nums = {0,0,1,1,1,2,2,3,3,4};
         int result = removeDuplicates(nums);
         System.out.println(result);
-        System.out.println(Arrays.toString(nums));
+
     }
 }
