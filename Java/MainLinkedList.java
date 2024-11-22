6bbb770cb8c75d547338d9efb69f4c4f0ea740eb
Masud Rana <masud.rana@apsissolutions.com>
2024-11-22 23:27:20 +0100

    Remove element from linked list

--- Java/MainLinkedList.java
+++ Java/MainLinkedList.java
@@ -1,42 +1,93 @@
-class Node{
+class Node {
     int data;
     Node next;
 }
 
-class LinkedList{
+class LinkedList {
     static Node head;
-    public void insert(int data){
+
+    public void insert(int data) {
 
         Node node = new Node();
         node.data = data;
 
-        if(head == null){
+        if (head == null) {
             head = node;
-        }else {
+        } else {
             Node n = head;
-            while (n.next != null){
+            while (n.next != null) {
                 n = n.next;
             }
             n.next = node;
         }
     }
 
-    public void print(){
+    public void initialInsert(int data) {
+        Node node = new Node();
+        node.data = data;
+
+        node.next = head;
+        head = node;
+
+    }
+
+    public void insertAt(int index, int data) {
+        Node node = new Node();
+        node.data = data;
+
+        Node n = head;
+        for (int i = 0; i < index - 1; i++) {
+            n = n.next;
+        }
+        node.next = n.next;
+        n.next = node;
+
+    }
+
+    public void deleteByValue(int value) {
+
+        if (head.data == value) {
+            head = head.next;
+            return;
+        }
+
+        Node n = head;
+        Node temp;
+        while (n.next != null) {
+            if(n.next.data == value){
+                temp = n.next;
+                n.next = temp.next;
+            }else {
+                n = n.next;
+            }
+
+        }
+
+    }
+
+    public void print() {
         Node node = head;
 
-        while (node.next != null){
+        while (node.next != null) {
             System.out.println(node.data);
             node = node.next;
         }
         System.out.println(node.data);
     }
 }
+
 public class MainLinkedList {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         list.insert(5);
         list.insert(8);
+        list.insert(8);
         list.insert(12);
+
+        list.initialInsert(20);
+        // list.insertAt(2,16);
+
+        list.deleteByValue(8);
 
         list.print();
 
