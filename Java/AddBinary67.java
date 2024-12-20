624cc9f57047457f8eb2ad395cf90128edcb7f64
Masud Rana <masud.rana@apsissolutions.com>
2024-12-20 17:57:36 +0100

    Binary add problem solved

--- Java/AddBinary67.java
+++ Java/AddBinary67.java
@@ -2,9 +2,35 @@ public class AddBinary67 {
 
     public static String addBinary(String a, String b) {
 
-        int sum = Integer.parseInt(a,2) + Integer.parseInt(b,2);
+        int lengthA = a.length() - 1;
+        int lengthB = b.length() - 1;
+        int carry = 0, base = 2;
 
-        return Integer.toBinaryString(sum);
+        StringBuilder result = new StringBuilder();
+
+        while (lengthA >= 0 || lengthB >= 0){
+            int t1 = 0, t2 = 0, sum = 0;
+
+            if(lengthA >= 0)
+                t1 = a.charAt(lengthA--) - '0';
+
+            if(lengthB >= 0)
+                t2 = b.charAt(lengthB--) - '0';
+
+            sum = t1 + t2 + carry;
+
+            if(sum >= base){
+                carry = 1;
+                sum = sum - base;
+            }else{
+                carry = 0;
+            }
+            result.append(sum);
+        }
+        if(carry == 1)
+            result.append(carry);
+
+        return result.reverse().toString();
     }
     public static void main(String[] args) {
 
