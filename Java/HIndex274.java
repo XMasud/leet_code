9803ac78234f84dac9c254dc6af05bc89688d53e
Masud Rana <masud.rana@apsissolutions.com>
2024-12-29 17:18:13 +0100

    HIndex problem solved

--- Java/HIndex274.java
+++ Java/HIndex274.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class HIndex274 {
+import java.util.Arrays;
+
+public class HIndex274 {
+    public static void main(String[] args) {
+        int[] citations = {3, 0, 6, 1, 5};
+        int result = hIndex(citations);
+        System.out.println(result);
+    }
+
+    private static int hIndex(int[] citations) {
+
+        int[] result = new int[citations.length + 1];
+
+        for (int citation : citations) {
+            if (citation > citations.length) {
+                result[citations.length]++;
+            } else {
+                result[citation]++;
+            }
+        }
+
+        int count = 0;
+        for (int i = citations.length; i >= 0; i--) {
+            count = count + result[i];
+            if (count >= i) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
 }
