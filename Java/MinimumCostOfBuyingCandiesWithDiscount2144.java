c90f7b99a68cba66704849924b75d24c0b12a421
Masud Rana <masud.rana@apsissolutions.com>
2025-01-10 19:52:27 +0100

    Cost of buying candies problem optmized

--- Java/MinimumCostOfBuyingCandiesWithDiscount2144.java
+++ Java/MinimumCostOfBuyingCandiesWithDiscount2144.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class MinimumCostOfBuyingCandiesWithDiscount2144 {
     public static void main(String[] args) {
-        int[] cost = {6,5,7,9,2,2};
+        int[] cost = {6, 5, 7, 9, 2, 2};
         int result = minimumCost(cost);
         System.out.println(result);
     }
@@ -13,9 +13,9 @@ public class MinimumCostOfBuyingCandiesWithDiscount2144 {
 
         int count = 0;
         int pay = 0;
-        for (int i = cost.length - 1; i >= 0 ; i--) {
+        for (int i = cost.length - 1; i >= 0; i--) {
             count++;
-            if(!(count % 3 == 0)){
+            if (!(count % 3 == 0)) {
                 pay = pay + cost[i];
             }
         }
