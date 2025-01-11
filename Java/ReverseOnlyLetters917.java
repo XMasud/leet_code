d70d66d5f63fff40f1809a89b06a2ed3715e31b3
Masud Rana <masud.rana@apsissolutions.com>
2025-01-11 23:28:46 +0100

    Reverse only letter problem solved

--- Java/ReverseOnlyLetters917.java
+++ Java/ReverseOnlyLetters917.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class ReverseOnlyLetters917 {
+import java.util.Arrays;
+
+public class ReverseOnlyLetters917 {
+    public static void main(String[] args) {
+        String s = "Test1ng-Leet=code-Q!";
+        String result = reverseOnlyLetters(s);
+    }
+
+    private static String reverseOnlyLetters(String s) {
+
+        if(s.isEmpty())
+            return null;
+        int start = 0;
+        int end = s.length() - 1;
+        char newStr[] = s.toCharArray();
+
+        while (start < end){
+            if(!Character.isLetter(newStr[end])){
+                end--;
+            } else if (!Character.isLetter(newStr[start])) {
+                start++;
+            } else if (Character.isLetter(newStr[end]) && Character.isLetter(newStr[start])){
+                char temp = newStr[start];
+                newStr[start] = newStr[end];
+                newStr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        System.out.println(newStr);
+
+        return String.valueOf(newStr);
+    }
 }
