e9969d50d7bb5198f51ea52566878cd02541f5b5
Masud Rana <masud.rana@apsissolutions.com>
2025-01-15 23:04:02 +0100

    Temo attack problem solved

---
+++ Java/TeemoAttacking495.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class TeemoAttacking495 {
+    public static void main(String[] args) {
+        int[] timeSeries = {1,4};
+        int duration = 2;
+        int result = findPoisonedDuration(timeSeries, duration);
+        System.out.println(result);
+    }
+
+    private static int findPoisonedDuration(int[] timeSeries, int duration) {
+
+        int total = duration;
+
+        for (int i = 1; i < timeSeries.length; i++) {
+
+            int timeDifference = timeSeries[i] - timeSeries[i - 1];
+
+            total += Math.min(timeDifference, duration);
+        }
+
+        return total;
+    }
+}
