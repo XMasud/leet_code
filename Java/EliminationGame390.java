4df1e77458ec2d1f8c6049d725e3cbcd0abd751f
Masud Rana <masud.rana@apsissolutions.com>
2025-01-23 23:31:18 +0100

    Commented

--- Java/EliminationGame390.java
+++ Java/EliminationGame390.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class EliminationGame390 {
+import java.util.HashSet;
+
+public class EliminationGame390 {
+    public static void main(String[] args) {
+        int n = 9;
+        int result = lastRemaining(n);
+    }
+    public static int lastRemaining(int n) {
+
+        if(n == 1)
+            return 1;
+        int num = -1;
+
+        HashSet<Integer> result = new HashSet<>();
+        for (int i = 1; i <= n ; i++) {
+            result.add(i);
+        }
+
+
+
+        return num;
+    }
+
+    /*public static HashSet<Integer> getLastValue(HashSet<Integer> result, int flag){
+
+        if(result.size() == 2)
+            return result;
+
+        for (int i =1; i <= flag; i++) {
+            if(flag % 2 == 1){
+                if(i % 2 == 1)
+                    result.remove(i);
+            }else {
+                if(i % 2 == 0)
+                    result.remove(i);
+            }
+        }
+        flag = result.size();
+
+        return getLastValue(result, flag);
+    }*/
 }
