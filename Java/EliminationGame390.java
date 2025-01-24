89ff40c49ac1fbda5cc6d85cdcba4b9e63381497
Masud Rana <masud.rana@apsissolutions.com>
2025-01-24 23:38:17 +0100

    Elemination problem solved

--- Java/EliminationGame390.java
+++ Java/EliminationGame390.java
@@ -5,38 +5,42 @@ public class EliminationGame390 {
         int n = 9;
         int result = lastRemaining(n);
     }
+
     public static int lastRemaining(int n) {
 
-        if(n == 1)
+        if (n == 1)
             return 1;
         int num = -1;
 
         HashSet<Integer> result = new HashSet<>();
-        for (int i = 1; i <= n ; i++) {
+        for (int i = 1; i <= n; i++) {
             result.add(i);
         }
 
-
+        System.out.println(getLastValue(result, 9));
 
         return num;
     }
 
-    /*public static HashSet<Integer> getLastValue(HashSet<Integer> result, int flag){
+    public static HashSet<Integer> getLastValue(HashSet<Integer> result, int flag) {
 
-        if(result.size() == 2)
+        if (result.size() == 2)
             return result;
 
-        for (int i =1; i <= flag; i++) {
-            if(flag % 2 == 1){
-                if(i % 2 == 1)
+
+        for (int i = 1; i <= flag; i++) {
+            if (flag % 2 == 1) {
+                if (i % 2 == 1)
                     result.remove(i);
-            }else {
-                if(i % 2 == 0)
+            } else { // 2 4 6 8
+                if (i % 2 == 0)
                     result.remove(i);
             }
         }
+
+
         flag = result.size();
 
         return getLastValue(result, flag);
-    }*/
+    }
 }
