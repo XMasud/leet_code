cdf60562b1e6b085a20c912e9b9bd67b9e1ac8e3
Masud Rana <masud.aiub.cse@gmail.com>
2025-02-15 21:00:35 +0100

    Check distance problem solved

--- Java/CheckDistancesBetweenSameLetters2239.java
+++ Java/CheckDistancesBetweenSameLetters2239.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public interface CheckDistancesBetweenSameLetters2239 {
+    public static void main(String[] args) {
+        String s = "rryzglzgyl";
+        int[] distance = {1,9,22,36,3,20,2,42,47,5,35,3,11,37,14,37,44,0,15,9,19,44,16,32,5,2};
+        boolean result = checkDistances(s,distance);
+        System.out.println(result);
+    }
+
+    public static boolean checkDistances(String s, int[] distance) {
+
+        if(s.length() <= 1)
+            return false;
+
+        HashMap<Integer,Integer> result = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            int val = s.charAt(i) - 'a';
+
+            if(result.containsKey(val)){
+                int temp = result.get(val);
+                result.put(val,i - temp);
+            }else{
+                result.put(val,i+1);
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+
+           if(result.containsKey(i)){
+               if(result.get(i) != distance[i]){
+                   return false;
+               }
+           }
+        }
+        return true;
+    }
+
 }
