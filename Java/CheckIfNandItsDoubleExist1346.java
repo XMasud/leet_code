3f19d0109eed59dc13b4c5a1659930afaa19185f
Masud Rana <masud.aiub.cse@gmail.com>
2025-03-16 22:49:36 +0100

    check if n problem solved

--- Java/CheckIfNandItsDoubleExist1346.java
+++ Java/CheckIfNandItsDoubleExist1346.java
@@ -3,6 +3,8 @@ import java.util.HashMap;
 public class CheckIfNandItsDoubleExist1346 {
     public static void main(String[] args) {
         int[] arr = {10,2,5,3};
+        boolean result = checkIfExist(arr);
+        System.out.println(result);
     }
     public static boolean checkIfExist(int[] arr) {
 
@@ -10,11 +12,16 @@ public class CheckIfNandItsDoubleExist1346 {
         int counter = 0;
         for (int n: arr){
             counter++;
-            maps.put(counter,n);
+            maps.put(n,counter);
         }
 
+        counter = 0;
         for (int n: arr){
-            
+            if(maps.containsKey(n*2)){
+                if(counter != (maps.get(n*2) - 1))
+                    return true;
+            }
+            counter++;
         }
 
         return false;
