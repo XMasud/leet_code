8f0ff83c369e200809d1a8d6af8d1a7c4ceef4ee
Masud Rana <masud.aiub.cse@gmail.com>
2025-03-27 23:46:27 +0100

    Remove duplicate from first array solved

--- Java/RemoveDuplicateFromSortedArray80.java
+++ Java/RemoveDuplicateFromSortedArray80.java
@@ -1,27 +1,24 @@
+import java.util.Arrays;
+
 public class RemoveDuplicateFromSortedArray80 {
     public static void main(String[] args) {
         int[] nums = {0,0,1,1,1,1,2,3,3};
         int result = removeDuplicates(nums);
+        System.out.println(result);
     }
 
     public static int removeDuplicates(int[] nums) {
 
         int index = 0;
 
-        int last_value = -1;
-        int value_count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if(last_value != nums[i]){
-                last_value = nums[i];
-                value_count = 1;
+        for (int num: nums){
+            if(index < 2 || num != nums[index - 2]){
+                nums[index] = num;
                 index++;
-            }else {
-                value_count++;
-                if(value_count <= 2)
-                    index++;
             }
         }
 
         return index;
     }
+
 }
