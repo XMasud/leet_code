d2346be4c804ea9ebd1f8b28537e10beac975fb6
Masud Rana <masud.aiub.cse@gmail.com>
2025-04-26 01:44:53 +0200

    Number of 1 bits problem solved

---
+++ Java/NumberOf1Bits.java
@@ -0,0 +1,19 @@
+public class NumberOf1Bits {
+    public static void main(String[] args) {
+        int n = 11;
+        int result = hammingWeight(n);
+    }
+
+    private static int hammingWeight(int n) {
+
+        String binaryString = Integer.toBinaryString(n);
+        int count = 0;
+
+        for (char c: binaryString.toCharArray()){
+            if(c == '1')
+                count++;
+        }
+
+        return count;
+    }
+}
