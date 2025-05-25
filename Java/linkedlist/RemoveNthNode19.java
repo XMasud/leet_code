bbcafdc8fe0ae81411ee2a4e36f46339fd298819
Masud Rana <masud.aiub.cse@gmail.com>
2025-05-26 00:27:00 +0200

    Remove nth node from Linked list

---
+++ Java/linkedlist/RemoveNthNode19.java
@@ -0,0 +1,24 @@
+package linkedlist;
+
+public class RemoveNthNode19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode current = head;
+        int size = 0;
+
+        while(current != null){
+            current = current.next;
+            size++;
+        }
+
+        if(size - n == 0) return head.next;
+
+        current = head;
+        for(int i=1; i < size - n; i++){
+            current = current.next;
+        }
+        current.next = current.next.next;
+
+        return head;
+    }
+}
