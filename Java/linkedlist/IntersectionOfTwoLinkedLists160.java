976b3fac03cfb1d837c0ec3b1eea8cb92a6a9b37
Masud Rana <masud.aiub.cse@gmail.com>
2025-05-25 20:25:31 +0200

    Intersection problem solved

--- Java/linkedlist/IntersectionOfTwoLinkedLists160.java
+++ Java/linkedlist/IntersectionOfTwoLinkedLists160.java
@@ -1,4 +1,25 @@
 package linkedlist;
 
+import java.util.HashSet;
+
 public class IntersectionOfTwoLinkedLists160 {
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> nodeCollection = new HashSet<>();
+
+        ListNode currentA = headA;
+        ListNode currentB = headB;
+
+        while (currentA != null){
+            if(!nodeCollection.contains(currentA))
+                nodeCollection.add(currentA);
+            currentA = currentA.next;
+        }
+
+        while (currentB != null){
+            if(nodeCollection.contains(currentB))
+                return currentB;
+            currentB = currentB.next;
+        }
+        return null;
+    }
 }
