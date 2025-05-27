f6d2db3cc8f390eac97e4b3ea05d6b011dadf8c1
Masud Rana <masud.aiub.cse@gmail.com>
2025-05-27 11:11:11 +0200

    Minimum array operaration problem solved

---
+++ Java/MinimumDeletionsMostKDistinctCharacters3545.java
@@ -0,0 +1,14 @@
+public class MinimumDeletionsMostKDistinctCharacters3545 {
+    public static void main(String[] args) {
+        String s = "abc";
+        int k = 2;
+
+    }
+    public static int minDeletion(String s, int k) {
+        int count = 0;
+
+
+
+        return count;
+    }
+}
