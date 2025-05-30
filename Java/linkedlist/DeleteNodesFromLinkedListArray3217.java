a1dcb3d05fa5714ed614f4d4de0f41aa610d3e53
Masud Rana <masud.aiub.cse@gmail.com>
2025-05-31 00:14:59 +0200

    Delete nodees from linked list problwm solved

---
+++ Java/linkedlist/DeleteNodesFromLinkedListArray3217.java
@@ -0,0 +1,28 @@
+package linkedlist;
+
+import java.util.HashSet;
+
+public class DeleteNodesFromLinkedListArray3217 {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+
+        HashSet<Integer> result = new HashSet<>();
+        for (int num: nums){
+            result.add(num);
+        }
+
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+
+        ListNode current = dummyNode;
+
+        while (current.next != null){
+            if(result.contains(current.next.val)){
+                current.next = current.next.next;
+            }else{
+                current = current.next;
+            }
+        }
+
+        return dummyNode.next;
+    }
+}
