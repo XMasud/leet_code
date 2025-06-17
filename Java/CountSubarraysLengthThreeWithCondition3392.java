a50f276b860e14e57c9dca02b86407dfd15e146a
Masud Rana <masud.aiub.cse@gmail.com>
2025-06-17 20:27:11 +0200

    Cound subarry problem solved

--- Java/CountSubarraysLengthThreeWithCondition3392.java
+++ Java/CountSubarraysLengthThreeWithCondition3392.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class CountSubarraysLengthThreeWithCondition3392 {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1};
+        int result = countSubarrays(nums);
+        System.out.println(result);
+    }
+
+    public static int countSubarrays(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (((nums[i] + nums[i + 2]) * 2 == nums[i + 1]))
+                count++;
+        }
+
+        return count;
+    }
 }
