8367a47a37eb86a1da77ef7c4140f93ab9ef1542
Masud Rana <masud.aiub.cse@gmail.com>
2025-09-29 23:59:33 +0200

    Single number 2 problem solved

--- Java/Raough.java
+++ Java/Raough.java
@@ -1,22 +1,41 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Raough {
     public static void moveZeroes(int[] nums) {
 
-        if(nums.length < 2)
-            return;
+        HashMap<String, Integer> map = new HashMap<>();
 
-        int index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i] != 0){
-                nums[index] = nums[i];
-                index++;
-            }
-        }
+        map.put("Alice",5);
+        map.put("Max",15);
+        map.put("Marc",25);
+        map.put("Jack",1);
+        map.put("Keny",51);
 
-        for (int i = index; i < nums.length; i++) {
-            nums[i] = 0;
-        }
+        Map<String,Integer> sortedMap = map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1,e2) -> e1,
+                        LinkedHashMap::new
+                ));
+
+        Map<String,Integer> desSortValue = map.entrySet()
+                        .stream()
+                        .sorted(Map.Entry.<String,Integer>comparingByValue().reversed())
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                (e1,e2)->e1,
+                                LinkedHashMap::new
+                        ));
+
+        System.out.println(desSortValue);
     }
     public static void main(String[] args) {
         int[] nums = {0,1,0,3,12};
