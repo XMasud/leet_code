32445667d0b68569d2d97dc04cce83aa45cde1ee
Masud Rana <masud.aiub.cse@gmail.com>
2026-01-31 23:59:30 +0100

    2270 added

---
+++ Java/NumberOfWaysSplitArray2270.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class NumberOfWaysSplitArray2270 {
+    public static void main(String[] args) {
+        int[] nums = {10,4,-8,7};
+        int result = waysToSplitArray(nums);
+        System.out.println(result);
+    }
+
+    private static int waysToSplitArray(int[] nums) {
+        int result = 0;
+        int left = 0;
+
+        int sum = Arrays.stream(nums).sum();
+
+        for(int i=0; i < nums.length - 1; i++){
+            left = left + nums[i];
+            if(left >= sum - left)
+                result++;
+        }
+        return result;
+    }
+}
