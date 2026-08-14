d1a7917c003a97a5b247edb734b3d81e8bad76a6
Masud Rana <masud.aiub.cse@gmail.com>
2026-08-14 23:41:18 +0200

    Merger interval v1

--- Java/MergeIntervals56.java
+++ Java/MergeIntervals56.java
@@ -5,39 +5,40 @@ import java.util.List;
 
 public class MergeIntervals56 {
     public static void main(String[] args) {
-        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        int[][] intervals = {{1,2},{4,6}};
         int[][] result = merge(intervals);
 
     }
     public static int[][] merge(int[][] intervals) {
 
         List<int[]> result = new ArrayList<>();
-
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
 
-        int currentStart = intervals[0][0];
-        int currentEnd = intervals[0][1];
+
+        int previousStart = intervals[0][0];
+        int previousEnd = intervals[0][1];
 
         for (int i = 1; i < intervals.length; i++) {
 
-            int nextStart = intervals[i][0];
-            int nextEnd = intervals[i][1];
+            int newStart = 0;
+            int newEnd = 0;
 
-            if (currentStart < nextStart){
+            int currentStart = intervals[i][0];
+            int currentEnd = intervals[i][1];
 
-                result.add(new int[] {currentStart, currentEnd});
-
-                currentStart = nextStart;
-                currentEnd = nextEnd;
-            }else {
-                currentEnd = Math.max(currentEnd, nextEnd);
+            if(currentStart < previousEnd){
+                newStart = previousStart;
+            }else{
+                newStart = currentStart;
             }
+
+            newEnd = Math.max(currentEnd, previousEnd);
+
+            result.add(new int[]{newStart, newEnd});
         }
 
-        result.add(new int[] {currentStart, currentEnd});
+        System.out.println(Arrays.deepToString(result.toArray(new int[0][])));
 
-        System.out.println(result);
-        
         return result.toArray(new int[result.size()][]);
     }
 }
